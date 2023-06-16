626b4f0b3555c274a5aef746334d9981d2e1c75b
Eugene <eugenevasile@gmail.com>
2023-06-16 15:36:54 +0200

    Async call

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-const {checkInventory} = require('./library.js');
+const fs = require('fs');
+const promisifiedReadfile = require('./promisifiedReadfile');
 
-const order = [['sunglasses', 1905], ['bags', 2]];
+// Here we use fs.readfile() and callback functions:
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
 
-const handleSuccess = (succesfullOrder) => {
-    console.log(succesfullOrder);
-};
-  
-const handleFailure = (rejectedOrder) => {
-    console.log(rejectedOrder);
-};
+    if (err) throw err;
 
-// Write your code below:
-checkInventory(order)
-  .then(handleSuccess)
-  .catch(handleFailure);
+    let firstSentence = data;
 
-    
+    fs.readFile('./file2.txt', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let secondSentence = data;
+        console.log(firstSentence, secondSentence);
+    });
+});
+
+// Here we use native promises with our "promisified" version of readfile:
+let firstSentence;
+promisifiedReadfile('./file.txt', 'utf-8')
+    .then((data) => {
+        firstSentence = data;
+        return promisifiedReadfile('./file2.txt', 'utf-8');
+    })
+    .then((data) => {
+        let secondSentence = data;
+        console.log(firstSentence, secondSentence)
+    })
+    .catch((err) => { console.log(err) });
+
+// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+async function readFiles() {
+
+    let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8');
+    // let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8');
+
+    // console.log(firstSentence, secondSentence);
+}
+
+readFiles();
